e6a68036e4d047c43ede0faf535461c9a6a8f4b4
JIgnesh Parmar <jignesh301095@gmail.com>
2026-09-03 22:46:47 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,11 @@
+class Config:
+    name = "Jignesh"
+    valid_email = "jignesh.p@yopmail.com"
+    valid_password = "abc@abc123"
+    valid_age = 30
+    product_name="iPhone"
+    product_quantity="2"
+    total_price="$246.40"
+
+    in_valid_email = "jignesh@abc.com"
+    in_valid_password = "Pas@123"
